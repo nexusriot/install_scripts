ae44e4a30dc3d35186387e394488254e0909125b
Vladislav Ananev <vananyev@hystax.com>
2024-02-18 15:02:09 +0400

    Base install for FreeBSD

---
+++ install_freebsd.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+pkg install \
+nano \
+portsnap \
+bash \
+bash-completion \
+fish \
+wget \
+curl \
+rtorrent \
+htop \
+mc \
+python3 \
+screen \
+screenfetch \
+neofetch \
+tmux \
+autossh \
+deco-misc \
+ncdu \
+cmatrix \
+peco \
+netcat \
+socat \
+git \
+bpytop \
+sqlite3 \
+vim \
+nmap \
+nnn \
+lsd \
+dust \
+aria2 \
+bat \
+restic \
+rsync \
+bmon \
+exa \
+duf \
+onefetch \
+toybox \
+busybox \
+plocate
